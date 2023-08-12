db128de511eca557cda8d96bff9410e1badd0c82
RamsesCamacho1171 <victorcamacho1171@gmail.com>
2023-08-11 19:38:39 -0600

    creacion de sistema de rutas y validacion de login

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,19 +1,52 @@
-import React, {useState, createContext} from 'react'
-
+import React, { useState, createContext, useEffect } from 'react'
+import { setToken, getToken, removeToken } from "../api/token";
+import { useUser } from "../hooks";
 
 export const AuthContext = createContext({
-    auth: undefined,
-    login: () => null,
-    logout: () => null,
-  });
+  auth: undefined,
+  login: () => null,
+  logout: () => null,
+});
 
-export function AuthContext(props) {
-    const { children } = props;
-    const [auth, setAuth] = useState(undefined);
+export function AuthProvider(props) {
+  const { children } = props;
+  const [auth, setAuth] = useState(undefined);
+  const { getMe } = useUser();
 
-    return (
-        <div>
-        
-        </div>
-    )
+  useEffect(() => {
+    (async () => {
+      const token = getToken();
+      if (token) {
+        const me = await getMe(token);
+        setAuth({ token, me });
+      } else {
+        setAuth(null);
+      }
+    })();
+  }, []);
+
+  const login = async (token) => {
+    setToken(token);
+    const me = await getMe(token);
+    setAuth({ token, me });
+  };
+
+  const logout = () => {
+    if (auth) {
+      removeToken();
+      setAuth(null);
+    }
+  };
+
+  const valueContext = {
+    auth,
+    login,
+    logout,
+  };
+
+
+
+  return (
+    <AuthContext.Provider value={valueContext}>{children}</AuthContext.Provider>
+  );
 }
